ca3c5d79327c98250c0dd7b9f2cb7d22520f0ea1
natcz <nataliaczerep@gmail.com>
2023-01-31 21:37:16 +0100

    added server -client comunication

---
+++ server.js
@@ -0,0 +1,25 @@
+const io = require('socket.io')();
+
+
+
+
+io.on('connection', client => {
+
+    client.on('start_game', new_game);
+    client.on('existing_game', join_game);
+  
+    function join_game(game_code) {
+      client.join(game_code);
+      client.number = 2;
+      client.emit('init', 2);
+    }
+  
+    function new_game(game_code) {
+      client.join(game_code);
+      client.number = 1;
+      client.emit('init', 1);
+    }--
+
+  });
+  
+io.listen(process.env.PORT || 3000);
